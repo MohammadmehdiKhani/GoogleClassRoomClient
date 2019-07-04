79e540b5ca985478c62733a958dd95d1d38e1436
masud Shaker <amshaker79@gmail.com>
2019-07-04 03:12:20 -0700

    the errors of each field will be checked after filling them in login page

--- app/src/main/java/com/example/classroomclient/Activity/LoginActivity.java
+++ app/src/main/java/com/example/classroomclient/Activity/LoginActivity.java
@@ -3,10 +3,14 @@ package com.example.classroomclient.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.classroomclient.Domain.Request;
@@ -20,22 +24,26 @@ import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity
 {
-	
-	//SHOROOE COMITE 2
+
+
+    //SHOROOE COMITE 2
     ImageView imageView;
     Button choose_btn;
     private static final int PICK_IMAGE = 100;
     Uri imageUri;
     //PAYAN
 
+
+    EditText username,password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-		
-		
-        //SHOROOE COMITE 2
+
+
+//SHOROOE COMITE 2
         imageView = (ImageView) findViewById(R.id.imageView);
         choose_btn = (Button) findViewById(R.id.choose_btn);
 
@@ -46,11 +54,45 @@ public class LoginActivity extends AppCompatActivity
             }
         });
         //PAYAN
-		
-		
-	}
-	
-	
+
+
+        //Read from UI
+        username = findViewById(R.id.username_etx);
+        password = findViewById(R.id.password_etx);
+
+        username.setOnFocusChangeListener(new View.OnFocusChangeListener() {//BAAD AZ INKE KARBAR POR KARD FIELD HA RO BEHESH PAYAM NESHON MIDIM NA BAAD AZ ZADAN E DOKME
+
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+                if (username.getText().length() == 0) {
+                    username.setError("username can not be empty");
+                }
+
+            }
+
+        });
+        password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+                if (password.getText().length() == 0) {
+                    password.setError("password can not be empty");
+                }
+                else if(password.getText().length()<=
+
+                        5){
+                    password.setError("password should have more than 5 characters ");
+                }
+
+            }
+
+        });
+
+
+    }
+
     //SHOROOE COMITE 2
     private void openGallery() {
 
@@ -76,29 +118,21 @@ public class LoginActivity extends AppCompatActivity
     {
         try
         {
-            //Read from UI
-            EditText username = findViewById(R.id.username_etx);
-            EditText password = findViewById(R.id.password_etx);
 
-            if (username.length() == 0 || password.length() <= 5)
-            {
-                if (username.length() == 0)
-                {
-                    username.setError("Username can not be empty");
-                }
 
-                if (password.length() <= 5)
-                {
-                    password.setError("Password length can not be less than 5");
-                }
-                return;
+            String user = username.getText().toString().trim();
+            String pass = password.getText().toString().trim();
+            if (user.length() != 0 && pass.length() > 5) {// AGE MOSHKELI NABOOD VARED MISHE
+                Toast.makeText(this, "you can enter!", Toast.LENGTH_LONG).show();
+            } else {//AGE MOSHKELI BOOD BEHESH MIGIM HALLESH KON BAAD VARED SHO
+                Toast.makeText(this, "please fix the errors first", Toast.LENGTH_LONG).show();
             }
 
             //Create request
             ObjectMapper objectMapper = new ObjectMapper();
             RequestMeta meta = new RequestMeta("login");
             User userToRegister = new User(username.getText().toString(), password.getText().toString());
-            Request request = new Request(meta, userToRegister);
+            Request request = new Request(meta,userToRegister);
             String requestString = objectMapper.writeValueAsString(request);
 
             //Send request and get response
@@ -118,7 +152,6 @@ public class LoginActivity extends AppCompatActivity
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString("username", username.getText().toString());
                 editor.commit();
-
                 Intent entranceIntent = new Intent(getBaseContext(), MainActivity.class);
                 startActivity(entranceIntent);
             }
