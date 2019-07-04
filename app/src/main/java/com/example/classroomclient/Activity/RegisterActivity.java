6fc14bbfc9713210d38f1b283d0a03ecbc735a98
masud Shaker <amshaker79@gmail.com>
2019-07-04 03:14:25 -0700

    the errors of each field will be checked after filling them in register page

--- app/src/main/java/com/example/classroomclient/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/classroomclient/Activity/RegisterActivity.java
@@ -1,10 +1,14 @@
 package com.example.classroomclient.Activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.classroomclient.Domain.Request;
@@ -18,8 +22,8 @@ import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity
 {
-	
-	
+
+
 //SHOROOE COMITE 2
     ImageView imageView;
     Button choose_btn;
@@ -29,13 +33,15 @@ public class RegisterActivity extends AppCompatActivity
 
 
 
+
+    EditText username,password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-		
-		
+
 //SHOROOE COMITE 2
         imageView = (ImageView) findViewById(R.id.imageView);
         choose_btn = (Button) findViewById(R.id.choose_btn);
@@ -47,8 +53,46 @@ public class RegisterActivity extends AppCompatActivity
             }
         });
         //PAYAN
-	}
-	
+
+        //Read from UI
+        username = findViewById(R.id.username_etx);
+        password = findViewById(R.id.password_etx);
+
+
+
+        username.setOnFocusChangeListener(new View.OnFocusChangeListener() {//BAAD AZ INKE KARBAR POR KARD FIELD HA RO BEHESH PAYAM NESHON MIDE NA BAAD AZ ZADAN E DOKME
+
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+                if (username.getText().length() == 0) {
+                    username.setError("username can not be empty");
+                }
+
+            }
+
+        });
+        password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+                if (password.getText().length() == 0) {
+                    password.setError("password can not be empty");
+                }
+                else if(password.getText().length()<=
+
+                        5){
+                    password.setError("password should have more than 5 characters ");
+                }
+
+            }
+
+        });
+
+    }
+
+
     //SHOROOE COMITE 2
     private void openGallery() {
 
@@ -70,29 +114,20 @@ public class RegisterActivity extends AppCompatActivity
 
     //PAYAN
 
-
-
     public void onRegisterClicked(View view)
     {
         try
         {
             //Read UI Value
-            EditText username = findViewById(R.id.username_etx);
-            EditText password = findViewById(R.id.password_etx);
 
-            if (username.length() == 0 || password.length() <= 5)
-            {
-                if (username.length() == 0)
-                {
-                    username.setError("Username can not be empty");
-                }
-
-                if (password.length() <= 5)
-                {
-                    password.setError("Password length can not be less than 5");
-                }
-                return;
+            String user = username.getText().toString().trim();
+            String pass = password.getText().toString().trim();
+            if (user.length() != 0 && pass.length() > 5) {// AGE MOSHKELI NABOOD VARED MISHE
+                Toast.makeText(this, "you can enter!", Toast.LENGTH_LONG).show();
+            } else {//AGE MOSHKELI BOOD BEHESH MIGIM HALLESH KON BAAD VARED SHO
+                Toast.makeText(this, "please fix the errors first", Toast.LENGTH_LONG).show();
             }
+
 
             //Create Request
             User userToRegister = new User(username.getText().toString(), password.getText().toString());
@@ -126,5 +161,6 @@ public class RegisterActivity extends AppCompatActivity
         }
     }
 }
+
 
 
